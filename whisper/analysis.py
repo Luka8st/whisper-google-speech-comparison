4dff84eb25446b7319f093d242cfd94e08bcacf4
Luka8st <luka.bursic5@gmail.com>
2025-07-10 20:53:49 +0200

    create analysis dirs

--- whisper/analysis.py
+++ whisper/analysis.py
@@ -1,4 +1,8 @@
 import analysis_helpers.diacritics_hm, analysis_helpers.visualisations, analysis_helpers.model_compare
+import os
+
+os.makedirs("whisper\\test\\whisper_outputs\\analysis_plots", exist_ok=True)
+os.makedirs("google\\analysis_plots", exist_ok=True)
 
 analysis_helpers.diacritics_hm.diacritic_confusion("whisper\\test\\whisper_outputs\\metrics.csv", "whisper\\test\\whisper_outputs\\analysis_plots\\diacritic_heatmap.png")
 analysis_helpers.visualisations.plot_metrics("whisper\\test\\whisper_outputs\\metrics.csv", "whisper\\test\\whisper_outputs\\analysis_plots")
