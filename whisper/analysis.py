2f9ef468b2a0814a5d18b878a75431c3369eddde
Luka8st <luka.bursic5@gmail.com>
2025-07-10 21:12:29 +0200

    added gitignore

--- whisper/analysis.py
+++ whisper/analysis.py
@@ -3,6 +3,7 @@ import os
 
 os.makedirs("whisper\\test\\whisper_outputs\\analysis_plots", exist_ok=True)
 os.makedirs("google\\analysis_plots", exist_ok=True)
+os.makedirs("comparison", exist_ok=True)
 
 analysis_helpers.diacritics_hm.diacritic_confusion("whisper\\test\\whisper_outputs\\metrics.csv", "whisper\\test\\whisper_outputs\\analysis_plots\\diacritic_heatmap.png")
 analysis_helpers.visualisations.plot_metrics("whisper\\test\\whisper_outputs\\metrics.csv", "whisper\\test\\whisper_outputs\\analysis_plots")
